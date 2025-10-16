ef9f71a324f3191e7974c892f69c6335da8a66f1
LangQi99 <2032771946@qq.com>
2025-10-16 19:57:46 +0800

    [rfuse3]: enhance vfs.sh with additional directory listing commands

--- project/rfuse3/examples/vfs.sh
+++ project/rfuse3/examples/vfs.sh
@@ -3,7 +3,11 @@ mkdir -p "$MOUNTPOINT"
 cargo build --example vfs
 cargo run --example vfs -- --mountpoint "$MOUNTPOINT" &
 sleep 2
+
+ls "$MOUNTPOINT"
 ls -l "$MOUNTPOINT"
+ls -al "$MOUNTPOINT"
+
 cat "$MOUNTPOINT/hello.txt"
 stat "$MOUNTPOINT"
 umount "$MOUNTPOINT"
