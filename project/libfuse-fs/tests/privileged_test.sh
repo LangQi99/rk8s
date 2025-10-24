ddb48fa64de77f748a8ee0ece2f2b9c42fd3b967
LangQi99 <2032771946@qq.com>
2025-10-25 00:38:37 +0800

    1

--- project/libfuse-fs/tests/privileged_test.sh
+++
@@ -1,3 +0,0 @@
-sudo -E env CARGO_HOME=$CARGO_HOME RUSTUP_HOME=$RUSTUP_HOME PATH=$PATH bash /home/langqi99/Shared/github/rk8s/project/libfuse-fs/tests/integration_test.sh
-sudo umount /home/langqi99/Shared/github/rk8s/project/libfuse-fs/tests/test_artifacts/work/passthrough/mnt
-sudo umount /home/langqi99/Shared/github/rk8s/project/libfuse-fs/tests/test_artifacts/work/overlay/mnt 
